122e3f446f7cb0ddda642c67fb1da981880a0885
yunfan yang <yunfanya@andrew.cmu.edu>
2024-10-22 23:57:14 -0400

    update

--- src/retriever/driver/rag.py
+++ src/retriever/driver/rag.py
@@ -131,7 +131,8 @@ def get_rag(query_id, doc2text, query2docs, top_n, shuffle):
     # Select the top-N document IDs
     top_doc_ids = doc_ids[:top_n]
 
-    print("doc2text", doc2text)
+    print("top_doc_ids", top_doc_ids)
+
     
     # Retrieve the text for each document ID, if ID not in doc2text, print a warning
     doc_texts = [doc2text[str(doc_id)] for doc_id in top_doc_ids if str(doc_id) in doc2text]
